8849b6eab0ae6776cccef598002bb653ec16712c
eltun veli <124363556+eltunv@users.noreply.github.com>
2023-02-09 10:55:57 +0400

    Yeni Laravel Dosyalari Eklendi

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\ElaqeController;
+use App\Http\controllers\MainController;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/eltun','App\Http\controllers\EltunController@index');
 
 Route::get('/telebe' , function(){
      return 'telebe ad';
 });
 
+Route::get('/selli', function(){
+    return 'Selli Xanim';
+});
+
 Route::get('/haqqinda','App\Http\controllers\HaqqindaController@index');
-Route::get('/elaqe', [ElaqeController::class , 'index']);
+Route::get('/elaqe','App\Http\controllers\ElaqeController@index');
 Route::get('/haqqinda/{ad}','App\Http\controllers\HaqqindaController@insan');
+Route::get('/main', [MainController::class , 'index']);
